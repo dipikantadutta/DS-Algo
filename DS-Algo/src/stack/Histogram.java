58edde77ae2c8509f6fe351845b2bf830aaf2635
DDUTTA3 <DDUTTA3@WGC100DSCTMH2.chennai.ford.com>
2019-04-27 18:35:22 +0530

    27th April

--- DS-Algo/src/stack/Histogram.java
+++ DS-Algo/src/stack/Histogram.java
@@ -1,8 +1,49 @@
 package stack;
 
+import java.util.*;
+
 public class Histogram {
 	public static void main(String []args)
     {
-	System.out.println("hi");
+		int arr[] = {6,2,5,4,5,1,6};
+		Histogram hist = new Histogram();
+		System.out.println(hist.maxHistogram(arr));
     }
+
+	public int maxHistogram(int[] arr) {
+		int area=0;
+		int maxArea = 0;
+		int i ;
+		Stack<Integer> stack = new Stack<>();
+		for (i=0;i<arr.length;)
+		{
+			if (stack.isEmpty() || arr[i]>=arr[stack.peek()] )
+			{
+				stack.push(i);
+				i++;
+			}
+			else
+			{
+				int top = stack.pop();
+				if (stack.isEmpty())
+					area = arr[top] * i;
+				else
+					area = arr[top] * (i-stack.peek()-1);
+			}
+			if (area>maxArea)
+				maxArea = area;
+		}
+		if (!stack.isEmpty())
+		{
+			int top = stack.pop();
+			if (stack.isEmpty())
+				area = arr[top]*i;
+			else
+				area = arr[top]*(i-stack.peek()-1);
+			if (area>maxArea)
+					maxArea = area;
+		}
+			
+		return maxArea;
+	}
 }
